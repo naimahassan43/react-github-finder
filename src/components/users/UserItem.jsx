9610e164fc3934e2d738c17806d8c261d8da4722
Naima Hassan <naima.hassan434@gmail.com>
2021-12-31 21:36:55 +0000

    Display Users

---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+function UserItem({ user: { login, avatar_url } }) {
+  return (
+    <div className="card shadow-md compact side bg-base-100">
+      <div className="flex-row items-center space-x-4 card-body">
+        <div>
+          <div className="avatar">
+            <div className="rounded-full h-14 w-14 shadow">
+              <img src={avatar_url} alt="Profile" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className="card-title">{login}</h2>
+          <Link
+            to={`/users/${login}`}
+            className="text-base-content text-opacity-40"
+          >
+            Visit Profile
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+export default UserItem;
