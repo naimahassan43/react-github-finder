00d2c47abc9f3a46700a3d0d35fdceddbb25e8b8
Naima Hassan <naima.hassan434@gmail.com>
2021-12-31 02:35:22 +0000

    Setup Tailwind and Daisy UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
+    <div className="bg-purple-500">
+      <h1 className="text-xl">hi</h1>
+      <button className="btn">click</button>
     </div>
   );
 }
